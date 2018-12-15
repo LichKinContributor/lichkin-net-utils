71569a29ea94f012063babb0dc8885672e806a00
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-12-15 14:01:18 +0800

    JSONFileReader.cs

---
+++ lichkin-net-utils/LichKin/Utils/JSONFileReader.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace LichKin.Utils
+{
+    /// <summary>
+    ///     JSON文件工具类
+    /// </summary>
+    public class JSONFileReader
+    {
+        /// <summary>
+        ///     读取JSON文件内容（单个）
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>
+        ///   键值对
+        /// </returns>
+        public static Dictionary<String, String> Read(String fileName)
+        {
+            return JsonConvert.DeserializeObject<Dictionary<String, String>>(FileReader.Read(fileName).Trim());
+        }
+
+        /// <summary>
+        ///     读取JSON文件内容（列表）
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>
+        ///   键值对列表
+        /// </returns>
+        public static List<Dictionary<String, String>> ReadArray(String fileName)
+        {
+            return JsonConvert.DeserializeObject<List<Dictionary<String, String>>>(FileReader.Read(fileName).Trim());
+        }
+    }
+}
