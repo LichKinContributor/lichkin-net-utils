69fadf3c6aed139a614e54682f77ce6106d42eaf
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-12-15 13:48:01 +0800

    FileReader.cs

---
+++ lichkin-net-utils/LichKin/Utils/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace LichKin.Utils
+{
+    /// <summary>
+    ///     文件读取工具类
+    /// </summary>
+    public class FileReader
+    {
+        /// <summary>
+        ///     读取文件内容
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>
+        ///   文件内容
+        /// </returns>
+        public static String Read(String fileName)
+        {
+            String content;
+            using (StreamReader sr = new StreamReader(fileName, Encoding.UTF8))
+            {
+                content = sr.ReadToEnd();
+            }
+            return content;
+        }
+    }
+}
